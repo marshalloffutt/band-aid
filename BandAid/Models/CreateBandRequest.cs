f2d76446dd0560c354c8b431a5c8f0d8472da652
unknown <marshalloffutt@gmail.com>
2019-07-15 15:41:54 -0500

    writes band model, repository, and create request. adds crud methods to repository

---
+++ BandAid/Models/CreateBandRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BandAid.Models
+{
+    public class CreateBandRequest
+    {
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public string Description { get; set; }
+        public string LogoUrl { get; set; }
+        public DateTime DateCreated { get; set; }
+        public bool Inactive { get; set; }
+    }
+}

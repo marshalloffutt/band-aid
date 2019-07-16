5b6c02457b12a2faad6008321e5149e2fd5cc062
Marshall Offutt <marshalloffutt@gmail.com>
2019-07-16 14:04:49 -0500

    bandmember model, repo, controller, http methods

---
+++ BandAid/Controllers/BandMembersController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BandAid.Data;
+using BandAid.Models;
+using BandAid.Validators;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BandAid.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BandMembersController : ControllerBase
+    {
+        readonly BandMemberRepository _bandMemberRepository;
+
+        public BandMembersController()
+        {
+            _bandMemberRepository = new BandMemberRepository();
+        }
+
+        [HttpGet]
+        public ActionResult GetAllBandMembers()
+        {
+            var bandmembers = _bandMemberRepository.GetAll();
+
+            return Ok(bandmembers);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetSingleBandMember(int id)
+        {
+            var bandMember = _bandMemberRepository.GetSingle(id);
+
+            return Ok(bandMember);
+        }
+
+        [HttpPost("register")]
+        public ActionResult AddBandMember(CreateBandMemberRequest createRequest)
+        {
+            var newBandMember = _bandMemberRepository.AddBandMember(
+                createRequest.MusicianId,
+                createRequest.BandId);
+
+            return Created($"api/bandmember/{newBandMember.Id}", newBandMember);
+        }
+
+        [HttpDelete("delete/{bandMemberId}")]
+        public ActionResult delete(int bandMemberId)
+        {
+            var bandMemberToDelete = _bandMemberRepository.DeleteBandMember(bandMemberId);
+
+            return Ok(bandMemberToDelete);
+        }
+    }
+}
---
+++ BandAid/Models/BandMember.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BandAid.Models
+{
+    public class BandMember
+    {
+        public int Id { get; set; }
+        public int MusicianId { get; set; }
+        public int BandId { get; set; }
+    }
+}

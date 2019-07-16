5816529cc20e200385ad5100255f9f719a7cc1d9
unknown <marshalloffutt@gmail.com>
2019-07-15 21:05:56 -0500

    Finishes postings model, repository, controller, full crud methods

---
+++ BandAid/Controllers/PostingsController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BandAid.Data;
+using BandAid.Models;
+using BandAid.Validators;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BandAid.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostingsController : ControllerBase
+    {
+        readonly PostingRepository _postingRepository;
+
+        public PostingsController()
+        {
+            _postingRepository = new PostingRepository();
+        }
+
+        [HttpGet]
+        public ActionResult GetAllPostings()
+        {
+            var postings = _postingRepository.GetAll();
+
+            return Ok(postings);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetSinglePosting(int id)
+        {
+            var posting = _postingRepository.GetSingle(id);
+
+            return Ok(posting);
+        }
+
+        [HttpPost("register")]
+        public ActionResult AddPosting(CreatePostingRequest createRequest)
+        {
+            var newPosting = _postingRepository.AddPosting(
+                createRequest.InstrumentRequested,
+                createRequest.Description,
+                createRequest.Closed);
+
+            return Created($"api/posting/{newPosting.Id}", newPosting);
+        }
+
+        [HttpPut("update/{id}")]
+        public ActionResult update(Posting posting)
+        {
+            var updatedPosting = _postingRepository.Update(posting);
+
+            return Ok(updatedPosting);
+        }
+
+        [HttpDelete("delete/{postingId}")]
+        public ActionResult delete(int postingId)
+        {
+            var postingToDelete = _postingRepository.DeletePosting(postingId);
+
+            return Ok(postingToDelete);
+        }
+    }
+}

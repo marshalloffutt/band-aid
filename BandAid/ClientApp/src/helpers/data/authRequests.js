6aadc2bf969325d97a039257451d84c62351c86e
Marshall Offutt <marshalloffutt@gmail.com>
2019-07-19 09:05:18 -0500

    adds axios, authRequests

---
+++ BandAid/ClientApp/src/helpers/data/authRequests.js
@@ -0,0 +1,40 @@
+/* eslint-disable max-len */
+import firebase from 'firebase';
+import axios from 'axios';
+
+axios.interceptors.request.use((request) => {
+  const token = sessionStorage.getItem('token');
+
+  if (token != null) {
+    request.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return request;
+}, err => Promise.reject(err));
+
+axios.interceptors.response.use(response => response, (errorResponse) => {
+  console.error('Blew up');
+});
+
+const registerUser = user => firebase.auth().createUserWithEmailAndPassword(user.email, user.password);
+
+const loginUser = user => firebase.auth().signInWithEmailAndPassword(user.email, user.password).then((cred) => {
+  getCurrentUserJwt();
+});
+
+const logoutUser = () => firebase.auth().signOut();
+
+const getUid = () => firebase.auth().currentUser.uid;
+
+const getCurrentUserJwt = () => firebase
+  .auth()
+  .currentUser.getIdToken()
+  .then(token => sessionStorage.setItem('token', token));
+
+export default {
+  getUid,
+  loginUser,
+  logoutUser,
+  registerUser,
+  getCurrentUserJwt,
+};

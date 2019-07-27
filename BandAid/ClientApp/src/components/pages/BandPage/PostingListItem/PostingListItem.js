4c4ac92f1bedfd788e1a808310592ab6f0c95476
unknown <marshalloffutt@gmail.com>
2019-07-27 09:07:10 -0500

    gets the replies in state if user is in the band

--- BandAid/ClientApp/src/components/pages/BandPage/PostingListItem/PostingListItem.js
+++ BandAid/ClientApp/src/components/pages/BandPage/PostingListItem/PostingListItem.js
@@ -5,45 +5,28 @@ import roleTranslator from '../../../../helpers/roleTranslator';
 export default class PostingListItem extends Component {
   state = {
     posting: this.props.posting,
-    userInBand: false,
     replies: [],
   }
 
   rosterCheck = () => {
+    const postingId = this.state.posting.id;
     const userId = this.props.currentUser.id;
     const rosterArray = this.props.currentBand.musicians;
     rosterArray.forEach((musician) => {
       if (musician.id === userId) {
-        this.setState({ userInBand: true });
+        postingReplyRequests.getRepliesOnPosting(postingId)
+          .then((replies) => {
+            this.setState({ replies });
+          })
+          .catch((error) => {
+            console.error(error);
+          });
       }
     });
   }
 
-  compnentDidMount() {
-    this.rosterCheck()
-      .then(() => {
-        const postingId = this.state.posting.id;
-        const { userInBand } = this.state;
-        if (userInBand === true) {
-          postingReplyRequests.getRepliesOnPosting(postingId)
-            .then((replies))
-        }
-      })
-  }
-
   componentDidMount() {
     this.rosterCheck();
-    const postingId = this.state.posting.id;
-    const { userInBand } = this.state;
-    if (userInBand === true) {
-      postingReplyRequests.getRepliesOnPosting(postingId)
-        .then((replies) => {
-          this.setState({ replies });
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    }
   }
 
   render() {

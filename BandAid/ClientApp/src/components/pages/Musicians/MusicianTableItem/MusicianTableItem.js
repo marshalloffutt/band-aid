693ed5b532b58bd3918f97e9666b23ec1e1c18f2
unknown <marshalloffutt@gmail.com>
2019-08-06 21:02:01 -0500

    disable buttons on click

--- BandAid/ClientApp/src/components/pages/Musicians/MusicianTableItem/MusicianTableItem.js
+++ BandAid/ClientApp/src/components/pages/Musicians/MusicianTableItem/MusicianTableItem.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react';
 import { Button } from 'reactstrap';
 
 export default class MusicianTableItem extends Component {
+  state = {
+    addButtonDisabled: false,
+  }
+
   render() {
+    const { addButtonDisabled } = this.state;
     const { musician, addBandMember } = this.props;
 
     const addBandMemberEvent = () => {
+      this.setState({ addButtonDisabled: true });
       const musicianId = musician.id;
       addBandMember(musicianId);
     };
 
     return (
       <tr>
-        <td><Button color="danger" onClick={addBandMemberEvent}>Add</Button></td>
+        <td><Button color="danger" disabled={addButtonDisabled} onClick={addBandMemberEvent}>Add</Button> </td>
         <td>{musician.firstName} {musician.lastName}</td>
         <td>{musician.instrument}</td>
         <td>{musician.city}, {musician.state}</td>
